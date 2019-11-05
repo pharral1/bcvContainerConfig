c230a3079a49db22d7f9e3f3f08958d4ccdaf788
Ubuntu <ubuntu@ip-172-31-43-111.ec2.internal>
2019-11-05 20:09:02 +0000

    Change build script to build from LoadScript

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd rest/crimeRestAPI2.0
 git pull origin containerConfig
-cd ../../db/BCV_Database
-git pull origin Docker-Branch
+cd ../../db/LoadScript
+git pull origin containerBranch
 cd ../..
 docker-compose build
