cc15310f57c57f375fd45b516845768fa89494f4
pharral1 <pharral1@umbc.edu>
2019-11-23 12:31:33 -0500

    Fix for missing libssl for mysqlclient

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+echo $1
+if [ $1 -eq 1 ]
+then
 cd rest/crimeRestAPI2.0
 git pull origin containerConfig
 cd ../../db/LoadScript
@@ -5,4 +8,5 @@ git pull origin containerBranch
 cd ../../app/react
 git pull origin development-branch
 cd ../..
+fi
 docker-compose build
