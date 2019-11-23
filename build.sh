e0463fab57630e82551311b151fa9c25c8c8797a
Ubuntu <ubuntu@ip-172-31-43-111.ec2.internal>
2019-11-23 03:08:46 +0000

    Add react app to docker

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ cd rest/crimeRestAPI2.0
 git pull origin containerConfig
 cd ../../db/LoadScript
 git pull origin containerBranch
+cd ../../app/react
+git pull origin master
 cd ../..
 docker-compose build
