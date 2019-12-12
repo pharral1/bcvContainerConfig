f413cedfeee86ebd88cf6853bbd6a4ab30ee87aa
Ubuntu <ubuntu@ip-172-31-43-111.ec2.internal>
2019-12-12 03:39:34 +0000

    Install script

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@ echo $1
 if [ $1 -eq 1 ]
 then
 cd rest/crimeRestAPI2.0
-git pull origin containerConfig
+git pull origin master
 cd ../../db/LoadScript
 git pull origin containerBranch
 cd ../../app/react
-git pull origin development-branch
+git pull origin master
 cd ../..
 fi
 docker-compose build
---
+++ install.sh
@@ -0,0 +1,10 @@
+cd rest
+git clone https://github.com/pharral1/crimeRestAPI2.0.git
+cd ..
+cd app
+git clone https://github.com/cleger97/447_React.git
+mv 447_react react
+cd ..
+cd db
+git clone https://github.com/jpicolo1/LoadScript.git
+cd ..
